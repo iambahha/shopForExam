f52adb0ad674fab9f2e3364770cc9a2cfb016099
Baktiyar Ishenov <b1963826@gmail.com>
2020-03-21 18:33:53 +0600

    finish

--- src/App.js
+++ src/App.js
@@ -1,11 +1,65 @@
-import React from 'react';
+import React, {Component} from 'react';
+import {connect} from "react-redux";
+import {NotificationContainer} from "react-notifications";
+import {Container} from "reactstrap";
+import {Route, Switch, withRouter} from "react-router-dom";
 
-function App() {
-  return (
-    <>
+import {logoutUser} from "./store/actions/usersActions";
+import {fetchCategories} from "./store/actions/categoriesActions";
+import {fetchProducts} from "./store/actions/productsActions";
 
-    </>
-  );
+import ProductsPage from "./containers/ProductsPage/ProductsPage";
+import Register from "./containers/Register/Register";
+import Login from "./containers/Login/Login";
+import ProductDetails from "./containers/ProductDetails/ProductDetails";
+import NewProduct from "./containers/NewProduct/NewProduct";
+import Toolbar from "./components/UI/Toolbar/Toolbar";
+
+import './App.css';
+import Spinner from "./components/UI/Spinner/Spinner";
+
+class App extends Component {
+	componentDidMount() {
+		this.props.fetchCategories();
+	};
+
+	render() {
+		if (this.props.loading || !this.props.categories) {
+			return <Spinner/>
+		}
+
+		return (
+			<div className="App">
+				<NotificationContainer/>
+				<header>
+					<Toolbar user={this.props.user} logout={this.props.logoutUser}/>
+				</header>
+				<Container>
+							<Switch>
+								<Route path="/" exact component={ProductsPage}/>
+								<Route path="/products" exact component={ProductsPage}/>
+								<Route path="/products/new" exact component={NewProduct}/>
+								<Route path="/products/:id" exact component={ProductDetails}/>
+								<Route path="/register" exact component={Register}/>
+								<Route path="/login" exact component={Login}/>
+								<Route render={() => <h1>Page Not Found</h1>}/>
+							</Switch>
+				</Container>
+			</div>
+		);
+	}
 }
 
-export default App;
+const mapStateToProps = state => ({
+	user: state.users.user,
+	categories: state.categories.categories,
+	loading: state.categories.loading
+});
+
+const mapDispatchToProps = dispatch => ({
+	logoutUser: () => dispatch(logoutUser()),
+	fetchCategories: () => dispatch(fetchCategories()),
+	fetchProducts: (query) => dispatch(fetchProducts(query))
+});
+
+export default withRouter(connect(mapStateToProps, mapDispatchToProps)(App));
--- src/index.js
+++ src/index.js
@@ -1,47 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {applyMiddleware, combineReducers, compose, createStore} from 'redux';
 import {Provider} from 'react-redux';
-import thunkMiddleware from 'redux-thunk';
-import {createBrowserHistory} from "history";
-import {ConnectedRouter, connectRouter, routerMiddleware} from "connected-react-router";
+import {ConnectedRouter} from 'connected-react-router';
 import * as serviceWorker from './serviceWorker';
+import store, {history} from "./store/configureStore";
+import App from './App';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
-import 'react-draft-wysiwyg/dist/react-draft-wysiwyg.css';
-import App from './App';
-import productsReducer from "./store/reducers/productsReducer";
-import categoriesReducer from "./store/reducers/categoriesReducer";
-import usersReducer from "./store/reducers/usersReducer";
-
-
-
-const history = createBrowserHistory();
-
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-
-const rootReducer = combineReducers({
-  router: connectRouter(history),
-  products: productsReducer,
-  categories: categoriesReducer,
-  users: usersReducer,
-});
-
-const middleware = [
-  thunkMiddleware,
-  routerMiddleware(history)
-];
-
-const enhancers = composeEnhancers(applyMiddleware(...middleware));
-
-const store = createStore(rootReducer, enhancers);
+import 'react-notifications/lib/notifications.css';
+import './index.css';
 
 const app = (
-  <Provider store={store}>
-    <ConnectedRouter history={history}>
-      <App/>
-    </ConnectedRouter>
-  </Provider>
+    <Provider store={store}>
+        <ConnectedRouter history={history}>
+            <App/>
+        </ConnectedRouter>
+    </Provider>
 );
 
 ReactDOM.render(app, document.getElementById('root'));
