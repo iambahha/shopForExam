e68168e50c05beceab07a0aa5b19ad66c6542ef1
Baktiyar Ishenov <b1963826@gmail.com>
2020-03-21 18:30:43 +0600

    start

--- src/index.js
+++ src/index.js
@@ -1,17 +1,48 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import {applyMiddleware, combineReducers, compose, createStore} from 'redux';
+import {Provider} from 'react-redux';
+import thunkMiddleware from 'redux-thunk';
+import {createBrowserHistory} from "history";
+import {ConnectedRouter, connectRouter, routerMiddleware} from "connected-react-router";
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'react-draft-wysiwyg/dist/react-draft-wysiwyg.css';
+import App from './App';
+import productsReducer from "./store/reducers/productsReducer";
+import categoriesReducer from "./store/reducers/categoriesReducer";
+import usersReducer from "./store/reducers/usersReducer";
+
+
+
+const history = createBrowserHistory();
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const rootReducer = combineReducers({
+  router: connectRouter(history),
+  products: productsReducer,
+  categories: categoriesReducer,
+  users: usersReducer,
+});
+
+const middleware = [
+  thunkMiddleware,
+  routerMiddleware(history)
+];
+
+const enhancers = composeEnhancers(applyMiddleware(...middleware));
+
+const store = createStore(rootReducer, enhancers);
+
+const app = (
+  <Provider store={store}>
+    <ConnectedRouter history={history}>
+      <App/>
+    </ConnectedRouter>
+  </Provider>
 );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
+ReactDOM.render(app, document.getElementById('root'));
 serviceWorker.unregister();
